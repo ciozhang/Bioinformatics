f7f539340112e9f58897efe3e89c5d5551b3a93e
ciozhang <cio_zhang@163.com>
2017-09-04 17:14:03 +0800

    update week2

--- Finding_Hidden_Messages_in_DNA(Bioinformatics_I)/Week_2_Finding_Replication_Origins/functions.py
+++ Finding_Hidden_Messages_in_DNA(Bioinformatics_I)/Week_2_Finding_Replication_Origins/functions.py
@@ -14,3 +14,44 @@ def MinimumSkew(Genome):
             output.append(i)
     return output
 
+def HammingDistance(p, q):
+    assert len(p) == len(q)
+    count = 0
+    for i in range(len(p)):
+        if p[i] != q[i]:
+            count+=1
+    return count
+
+def ApproximatePatternMatching(Pattern, Text, d):
+    d = int(d)
+    positions=[]
+    k = len(Pattern)
+    for i in range(len(Text)-k+1):
+        if HammingDistance(Pattern,Text[i:i+k]) <= d:
+            positions.append(i)
+    return positions
+
+def ApproximatePatternCount(Pattern, Text, d):
+    d = int(d)
+    count = 0
+    k = len(Pattern)
+    for i in range(len(Text)-k+1):
+        if HammingDistance(Pattern,Text[i:i+k]) <= d:
+            count+=1
+    return count
+
+def Neighbors(Pattern, d):
+    d = int(d)
+    if d == 0:
+        return Pattern
+    if len(Pattern) == 1:
+        return ['A','C','G','T']
+    Neighborhood = []
+    SuffixNeighbors = Neighbors(Pattern[1:], d)
+    for Text in SuffixNeighbors:
+        if HammingDistance(Pattern[1:],Text) < d:
+            for x in ['A','C','G','T']:
+                Neighborhood.append(x+Text)
+        else:
+            Neighborhood.append(Pattern[0]+Text)
+    return Neighborhood
--- Finding_Hidden_Messages_in_DNA(Bioinformatics_I)/Week_2_Finding_Replication_Origins/program.py
+++ Finding_Hidden_Messages_in_DNA(Bioinformatics_I)/Week_2_Finding_Replication_Origins/program.py
@@ -1,6 +1,14 @@
-import sys
-#lines = sys.stdin.read().splitlines()
-
 from functions import *
 
+import sys
+lines = sys.stdin.read().splitlines()
+
 #print(' '.join(str(i) for i in MinimumSkew(lines[0])))
+
+#print(HammingDistance(lines[0],lines[1]))
+
+#print(' '.join(str(i) for i in ApproximatePatternMatching(lines[0],lines[1],lines[2])))
+
+#print(ApproximatePatternCount(lines[0],lines[1],lines[2]))
+
+print('\n'.join(str(i)for i in Neighbors(lines[0],lines[1])))
