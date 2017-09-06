e4a65a5713927ce46eb0f078cd64bd05bd9d0dce
ciozhang <cio_zhang@163.com>
2017-09-06 16:52:59 +0800

    update

--- Finding_Hidden_Messages_in_DNA(Bioinformatics_I)/Week_2_Finding_Replication_Origins/functions.py
+++ Finding_Hidden_Messages_in_DNA(Bioinformatics_I)/Week_2_Finding_Replication_Origins/functions.py
@@ -55,3 +55,129 @@ def Neighbors(Pattern, d):
         else:
             Neighborhood.append(Pattern[0]+Text)
     return Neighborhood
+
+
+def SymbolToNumber(Symbol):
+    if 'A' in Symbol:
+        return 0
+    elif 'C' in Symbol:
+        return 1
+    elif 'G' in Symbol:
+        return 2
+    elif 'T' in Symbol:
+        return 3
+
+def PatternToNumber(Pattern):
+    if len(Pattern)==1:
+        return SymbolToNumber(Pattern)
+    else:
+        symbol = Pattern[-1]
+        Prefix = Pattern[:-1]
+        return 4 * PatternToNumber(Prefix) + SymbolToNumber(symbol)
+
+def NumberToSymbol(index):
+    index = int(index)
+    if index == 0:
+        return 'A'
+    elif index == 1:
+        return 'C'
+    elif index == 2:
+        return 'G'
+    elif index == 3:
+        return 'T'
+
+def NumberToPattern(index, k):
+    index = int(index)
+    k = int(k)
+    if k == 1:
+        return NumberToSymbol(index)
+    else:
+        prefixIndex = index//4
+        r = index%4
+        symbol = NumberToSymbol(r)
+        return NumberToPattern(prefixIndex,k-1)+symbol
+
+def ComputingFrequenciesWithMismatches(Text, k, d):
+    FrequenciesArray = [0 for i in range(4**k)]
+    for i in range(len(Text)-k+1):
+        Pattern = Text[i:i+k]
+        Neighborhood = Neighbors(Pattern, d)
+        for s in Neighborhood:
+            j = PatternToNumber(s)
+            FrequenciesArray[j] += 1
+    return FrequenciesArray
+
+def ComputingWordsWithMismatches(Text, k, d):
+    k = int(k)
+    d = int(d)
+    FrequentPatterns = []
+    Neighborhoods = []
+    for i in range(len(Text)-k+1):
+        if d == 0:
+            Neighborhoods.append(Text[i:i+k])
+        else:
+            for j in Neighbors(Text[i:i+k],d):
+                Neighborhoods.append(j)
+    Index = [0 for i in range(len(Neighborhoods))]
+    Count = [0 for i in range(len(Neighborhoods))]
+    for i in range(len(Neighborhoods)):
+        Pattern = Neighborhoods[i]
+        Index[i] = PatternToNumber(Pattern)
+        Count[i] = 1
+    SortedIndex = sorted(Index)
+    for i in range(len(Neighborhoods)-1):
+        if SortedIndex[i] == SortedIndex[i+1]:
+            Count[i+1] = Count[i]+1
+    maxCount = max(Count)
+    for i in range(len(Neighborhoods)):
+        if Count[i] == maxCount:
+            Pattern = NumberToPattern(SortedIndex[i], k)
+            FrequentPatterns.append(Pattern)
+    return FrequentPatterns
+
+def ReverseComplement(Pattern):
+    revComp = '' # output variable
+    # your code here
+    for i in Pattern:
+        if i == 'A':
+            revComp += 'T'
+        elif i == 'T':
+            revComp += 'A'
+        elif i == 'C':
+            revComp += 'G'
+        elif i == 'G':
+            revComp += 'C'
+        else:
+            print('not ATCG, something wrong')
+    return revComp[::-1]
+
+def ComputingWordsWithMismatchesandReverseComplement(Text, k, d):
+    k = int(k)
+    d = int(d)
+    FrequentPatterns = []
+    Neighborhoods = []
+    for i in range(len(Text)-k+1):
+        if d == 0:
+            Neighborhoods.append(Text[i:i+k])
+            Neighborhoods.append(ReverseComplement(Text)[i:i+k])
+        else:
+            for j in Neighbors(Text[i:i+k],d):
+                Neighborhoods.append(j)
+            for j in Neighbors(ReverseComplement(Text)[i:i+k],d):
+                Neighborhoods.append(j)
+    Index = [0 for i in range(len(Neighborhoods))]
+    Count = [0 for i in range(len(Neighborhoods))]
+    for i in range(len(Neighborhoods)):
+        Pattern = Neighborhoods[i]
+        Index[i] = PatternToNumber(Pattern)
+        Count[i] = 1
+    SortedIndex = sorted(Index)
+    for i in range(len(Neighborhoods)-1):
+        if SortedIndex[i] == SortedIndex[i+1]:
+            Count[i+1] = Count[i]+1
+    maxCount = max(Count)
+    for i in range(len(Neighborhoods)):
+        if Count[i] == maxCount:
+            Pattern = NumberToPattern(SortedIndex[i], k)
+            FrequentPatterns.append(Pattern)
+    return FrequentPatterns
--- Finding_Hidden_Messages_in_DNA(Bioinformatics_I)/Week_2_Finding_Replication_Origins/program.py
+++ Finding_Hidden_Messages_in_DNA(Bioinformatics_I)/Week_2_Finding_Replication_Origins/program.py
@@ -3,12 +3,24 @@ from functions import *
 import sys
 lines = sys.stdin.read().splitlines()
 
-#print(' '.join(str(i) for i in MinimumSkew(lines[0])))
-
 #print(HammingDistance(lines[0],lines[1]))
 
 #print(' '.join(str(i) for i in ApproximatePatternMatching(lines[0],lines[1],lines[2])))
 
 #print(ApproximatePatternCount(lines[0],lines[1],lines[2]))
 
-print('\n'.join(str(i)for i in Neighbors(lines[0],lines[1])))
+#print('\n'.join(str(i)for i in Neighbors(lines[0],lines[1])))
+
+#print(ComputingFrequenciesWithMismatches('AAGCAAAGGTGGG', 2, 1))
+
+#k,d = lines[1].split(' ')
+#print(' '.join(str(i) for i in ComputingWordsWithMismatches(lines[0],k,d)))
+
+#k,d = lines[1].split(' ')
+#print(' '.join(str(i) for i in ComputingWordsWithMismatchesandReverseComplement(lines[0],k,d)))
+
+#string = ''
+#for i in range(1,len(lines)):
+#	string += lines[i]
+#print(' '.join(str(i) for i in MinimumSkew(string)))
+#print(string[3764800:3764900])
