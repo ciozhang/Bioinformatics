031cde1413bb839eac4a1a221cc85b3df29fc983
ciozhang <cio_zhang@163.com>
2017-09-11 10:32:51 +0800

    week3

--- Finding_Hidden_Messages_in_DNA(Bioinformatics_I)/Week_3_Hunting_for_Regulatory_Motifs/functions_3.py
+++ Finding_Hidden_Messages_in_DNA(Bioinformatics_I)/Week_3_Hunting_for_Regulatory_Motifs/functions_3.py
@@ -39,3 +39,66 @@ def MedianString(Dna,k):
             distance = DistanceBetweenPatternAndStrings(Pattern,Dna)
             Median = Pattern
     return Median
+
+def ProfileMostProbableKmer(Text, k, Matrix):
+    k = int(k)
+    Profile = -float('inf')
+    for i in range(len(Text)-k+1):
+        Pattern = Text[i:i+k]
+        profile = 1
+        for j in range(k):
+            if Pattern[j] == 'A':
+                profile *= float(Matrix[0][j])
+            elif Pattern[j] == 'C':
+                profile *= float(Matrix[1][j])
+            elif Pattern[j] == 'G':
+                profile *= float(Matrix[2][j])
+            elif Pattern[j] == 'T':
+                profile *= float(Matrix[3][j])
+            else:
+                print("not'ACGT', something wrong")
+        if Profile < profile:
+            Profile = profile
+            kmer = Pattern
+    return kmer
+
+def Profile(motifs):
+    Matrix = [[1. for i in range(len(motifs[0]))] for i in range(4)]
+    for i in range(len(motifs)):
+        for j in range(len(motifs[i])):
+            if len(motifs[0]) == 1:
+                last = i
+            else:
+                last = j
+            if motifs[i][j] == 'A':
+                Matrix[0][last] += 1
+            elif motifs[i][j] == 'C':
+                Matrix[1][last] += 1
+            elif motifs[i][j] == 'G':
+                Matrix[2][last] += 1
+            elif motifs[i][j] == 'T':
+                Matrix[3][last] += 1
+    for i in range(len(Matrix)):
+        for j in range(len(Matrix[0])):
+            Matrix[i][j] /= (len(motifs)+4)
+    return Matrix
+
+def Score(motifs):
+    Matrix = Profile(motifs)
+    score = (len(motifs)+1)*len(motifs[0])
+    for i in range(len(Matrix[0])):
+        score -= max(Matrix[j][i] for j in range(len(Matrix)))*(len(motifs)+4)
+    return score
+
+def GreedyMotifSearch(Dna,k,t):
+    k = int(k)
+    t = int(t)
+    assert t == len(Dna)
+    bestMotifs = [Dna[i][:k] for i in range(t)]
+    for i in range(len(Dna[0])-k+1):
+        motifs = [Dna[0][i:i+k]]
+        for j in range(1,t):
+            motifs.append(ProfileMostProbableKmer(Dna[j],k,Profile(motifs)))
+        if Score(motifs) < Score(bestMotifs):
+            bestMotifs = motifs
+    return bestMotifs
--- Finding_Hidden_Messages_in_DNA(Bioinformatics_I)/Week_3_Hunting_for_Regulatory_Motifs/program_3.py
+++ Finding_Hidden_Messages_in_DNA(Bioinformatics_I)/Week_3_Hunting_for_Regulatory_Motifs/program_3.py
@@ -4,15 +4,25 @@ from functions_3 import *
 import sys
 lines = sys.stdin.read().splitlines()
 Dna = []
-#for i in lines[1].split(' '):
-#	Dna.append(i)
-
 for i in lines[1:]:
-	Dna.append(i)
+    Dna.append(i)
+
+#for i in lines[1].split(' '):
+#   Dna.append(i)
 
 #k,d = lines[0].split(' ')
 #print(' '.join(str(i) for i in MotifEnumeration(Dna, k, d)))
 
 #print(DistanceBetweenPatternAndStrings(lines[0], Dna))
 
-print(MedianString(Dna,lines[0]))
+#print(MedianString(Dna,lines[0]))
+
+#Text = lines[0]
+#k = lines[1]
+#Matrix = []
+#for i in lines[2:]:
+#    Matrix.append(i.split(' '))
+#print(ProfileMostProbableKmer(Text, k, Matrix))
+
+k, t = lines[0].split(' ')
+print('\n'.join(str(i) for i in GreedyMotifSearch(Dna,k,t)))
